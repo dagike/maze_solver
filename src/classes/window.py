ac347f9a1f3d50562c0f51e557a6abce27247779
dagike <isaac20053@hotmail.com>
2025-02-25 19:17:27 -0500

    add: more initialization for the window

--- src/classes/window.py
+++ src/classes/window.py
@@ -3,9 +3,9 @@ from tkinter import Tk, BOTH, Canvas
 class Window():
   def __init__(self, width, height):
     self.root_widget = Tk()
-    self.root_widget.title = ""
-    self.canvas_widget = Canvas(self.root_widget, width=width, height=height)
-    self.canvas_widget.pack()
+    self.root_widget.title("Maze Solver")
+    self.canvas_widget = Canvas(self.root_widget, bg="white", width=width, height=height)
+    self.canvas_widget.pack(fill=BOTH, expand=1)
     self.isRunning = False
     self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -20,3 +20,6 @@ class Window():
 
   def close(self):
     self.isRunning = False
+
+  def draw_line(self, line, fill_colour):
+    line.draw(self.canvas_widget, fill_colour)
