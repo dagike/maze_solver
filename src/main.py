fdfe196a22408929473b2ba8b1b86a51f7a75759
dagike <isaac20053@hotmail.com>
2025-02-26 01:01:12 -0500

    add: initialization of window and screen

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 from classes.window import Window
+from classes.maze import Maze
 
 def main():
-  win = Window(800, 600)
+  num_rows = 12
+  num_cols = 16
+  margin = 50
+  screen_x = 800
+  screen_y = 600
+  cell_size_x = (screen_x - 2 * margin) / num_rows
+  cell_size_y = (screen_y - 2 * margin) / num_cols
+  win = Window(screen_x, screen_y)
+
+  maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 150)
+
   win.wait_for_close()
 
 main()
